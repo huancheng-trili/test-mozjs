168aa18713c46384a02d4b377f7be050e9047813
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-02-19 16:44:09 +0000

    add workflow and script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+tag="$1"
+
+if [[ -z "$tag" ]]; then
+   echo "mozjs tag is empty"
+   exit 1
+fi
+
+git clone https://github.com/servo/mozjs.git --branch "$tag" --single-branch
+cd mozjs && cat mozjs-sys/mozjs/build/autoconf/config.sub | sed 's/riscv64 | riscv64be/riscv64 | riscv64be | riscv64gc/g' > a && mv a mozjs-sys/mozjs/build/autoconf/config.sub
+
+env MOZJS_CREATE_ARCHIVE=1 CLANGFLAGS="-target riscv64-unknown-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld" BINDGEN_EXTRA_CLANG_ARGS='-target riscv64-unknown-linux-musl --sysroot=/ -Wno-unused-command-line-argument -fuse-ld=lld' CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_RUNNER="/usr/bin/qemu-riscv64-static" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_LINKER="riscv64-linux-musl-gcc" CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_MUSL_AR="riscv64-linux-musl-ar" MOZJS_FROM_SOURCE=1 cargo +1.82.0 build --target=riscv64gc-unknown-linux-musl
+
